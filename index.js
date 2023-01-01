f8c00e59e03f6264979b43b2fa7559a1ca41da4e
Rohith Reddy <kvrohith123@gmail.com>
2023-01-02 01:19:10 +0530

    Setup Mongo Session for not signing out even after server restart.

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
+
 
 app.use(express.urlencoded());
 app.use(cookieParser());
@@ -21,7 +23,7 @@ app.use(expressLayouts);
 app.set('view engine','ejs');
 app.set('views','./views');
 
- 
+ //mongo store is used to store the session cookie in the db
 app.use(session({
     name:'codeial',
     //TODO change the secret before deployment in production mode
@@ -30,7 +32,17 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000*60*100)
-    }
+    },
+    store: new MongoStore(
+
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        },
+        function (err){
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
 }));
 
 app.use(passport.initialize());
