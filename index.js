7730447771c9e25425cb3e3687b8bbdc5b3a9a62
Rohith Reddy <kvrohith123@gmail.com>
2023-01-10 01:28:34 +0530

    scss sass Post operatation start

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo');
+const sassMiddleware = require('node-sass-middleware');
 
+
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle:'extended',
+    prefix: '/css',
+
+}));
 
 app.use(express.urlencoded());
 app.use(cookieParser());
@@ -33,10 +43,10 @@ app.use(session({
     cookie: {
         maxAge: (1000*60*100)
     },
-    store: new MongoStore(
+    store: MongoStore.create(
 
         {
-            mongooseConnection: db,
+            mongoUrl: 'mongodb://localhost/codieal_development',
             autoRemove: 'disabled'
         },
         function (err){
